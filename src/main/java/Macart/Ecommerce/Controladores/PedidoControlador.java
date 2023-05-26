87e7b9113196a57d330acf75ca4e32ab403dcc41
Carlos Andrés Ruiz Hinestroza <123417384+carlosandresgoo@users.noreply.github.com>
2023-05-26 17:26:00 -0500

    carlos PedidosController y DTO

--- src/main/java/Macart/Ecommerce/Controladores/PedidoControlador.java
+++ src/main/java/Macart/Ecommerce/Controladores/PedidoControlador.java
@@ -3,15 +3,17 @@ package Macart.Ecommerce.Controladores;
 import Macart.Ecommerce.DTO.ClienteDTO;
 import Macart.Ecommerce.DTO.PedidoDTO;
 import Macart.Ecommerce.Modelos.Cliente;
+import Macart.Ecommerce.Modelos.Direccion;
 import Macart.Ecommerce.Modelos.Pedido;
+import Macart.Ecommerce.Modelos.PedidoMetodoDePago;
 import Macart.Ecommerce.Repositorio.ClienteRepositorio;
 import Macart.Ecommerce.Repositorio.PedidoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,7 +44,25 @@ public class PedidoControlador {
             .map(pedido -> new PedidoDTO(pedido))
             .collect(Collectors.toList());
 
-
     return  pedidosDTO;
+    }
+
+    @PostMapping("/api/pedidos")
+    public ResponseEntity<Object> crearPedidos(
+            @RequestParam Long clienteId,
+            @RequestParam LocalDateTime fechaDePedido,
+            @RequestParam double montoTotal,
+            @RequestParam String metodoDeEnvio,
+            @RequestParam String metodoDePago,
+            @RequestParam String codigoPostal) {
+
+        Cliente cliente = clienteRepositorio.findById(clienteId).orElse(null);
+
+        Pedido nuevoPedido = new Pedido(LocalDateTime.now(),false , montoTotal,metodoDeEnvio,PedidoMetodoDePago.valueOf(metodoDePago));
+        cliente.agregarPedido(nuevoPedido);
+        pedidoRepositorio.save(nuevoPedido);
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Direccion creada");
     }
 }
