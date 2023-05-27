c4337f0ed89e2889b55366ba4cf67426779a5822
Ivan San Miguel <ezequielivan2@gmail.com>
2023-05-27 14:32:44 -0300

    Eliminada la clase promocion con sus relaciones

--- src/main/java/Macart/Ecommerce/Repositorio/PromocionRepositorio.java
+++
@@ -1,9 +0,0 @@
-package Macart.Ecommerce.Repositorio;
-
-import Macart.Ecommerce.Modelos.Promocion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PromocionRepositorio extends JpaRepository<Promocion, Long> {
-}
