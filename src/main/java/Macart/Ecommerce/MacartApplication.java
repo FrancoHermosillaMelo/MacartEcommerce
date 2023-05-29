22f582eefdd8903cc39c93d35042052df7b46bc3
Ivan San Miguel <ezequielivan2@gmail.com>
2023-05-29 15:57:42 -0300

    Agregada data hardcodeada

--- src/main/java/Macart/Ecommerce/MacartApplication.java
+++ src/main/java/Macart/Ecommerce/MacartApplication.java
@@ -26,7 +26,7 @@ public CommandLineRunner initData(ClienteRepositorio clienteRepositorio, Direcci
 		// guardarclientes
 
 //		cliente1
-		Cliente cliente1 = new Cliente("Carlos","Andrés","Ruiz","Hinestroza","carlosandresgoo@gmail.com","6033603","322-567-8909","123");
+		Cliente cliente1 = new Cliente("Carlos","Andrés","Ruiz","Hinestroza","carlosandresgoo@gmail.com","322-567-8909","123");
 		clienteRepositorio.save(cliente1);
 
 		Direccion direccion1 = new Direccion("Calle-47a","50-05","Barrio obrero","Copacabana","Antioquia","12345");
@@ -38,15 +38,17 @@ public CommandLineRunner initData(ClienteRepositorio clienteRepositorio, Direcci
 		pedidoRepositorio.save(pedido1);
 		Comprobante comprobante1 = new Comprobante(PedidoMetodoDePago.TRANSFERENCIA,"Transportadora",LocalDateTime.now(),150000.00 );
 		comprobanteRepositorio.save(comprobante1);
+		cliente1.agregarComprobantes(comprobante1);
 
 
 //		cliente2
-		Cliente cliente2 =new Cliente("Ivan","Ezequiel","Miguel","Hernandez","Ivan@gmail.com","6543603","355-567-5555","321");
+		Cliente cliente2 =new Cliente("Ivan","Ezequiel","Miguel","Hernandez","Ivan@gmail.com","355-567-5555","321");
 		clienteRepositorio.save(cliente2);
 
 		Direccion direccion2 = new Direccion("Calle-100Z","100-05","mar de plata","Boca","buenos aires","54321");
 		cliente2.agregarDirecciones(direccion2);
 		direccionRepositorio.save(direccion2);
+
 		Pedido pedido2 = new Pedido(LocalDateTime.now(),false,320000.00,"domicilio", PedidoMetodoDePago.EFECTIVO);
 		cliente2.agregarPedido(pedido2);
 		pedidoRepositorio.save(pedido2);
