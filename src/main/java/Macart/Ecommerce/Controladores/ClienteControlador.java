b1516c0085f793b94e5edf98d9ac4730f32e8572
Ivan San Miguel <ezequielivan2@gmail.com>
2023-05-30 12:14:24 -0300

    Modificado controlador con su logica de negocios e implementacion de cliente

--- src/main/java/Macart/Ecommerce/Controladores/ClienteControlador.java
+++ src/main/java/Macart/Ecommerce/Controladores/ClienteControlador.java
@@ -3,9 +3,11 @@ package Macart.Ecommerce.Controladores;
 import Macart.Ecommerce.DTO.ClienteDTO;
 import Macart.Ecommerce.Modelos.Cliente;
 import Macart.Ecommerce.Repositorio.ClienteRepositorio;
+import Macart.Ecommerce.Servicios.ClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,36 +20,38 @@ import static java.util.stream.Collectors.toList;
 @RestController
 public class ClienteControlador {
     @Autowired
-    private ClienteRepositorio clienteRepositorio;
+    private ClienteServicio clienteServicio;
 
 
     @GetMapping("/api/clientes")
-    public List<ClienteDTO> obtenerClientes(){
-        return clienteRepositorio.findAll()
-                .stream().map(client ->
-                        new ClienteDTO(client)).collect(toList());  }
+    public ResponseEntity<Object> obtenerClientes(Authentication authentication){
+        if(clienteServicio.isAdmin(authentication)){
+            return new ResponseEntity<>(clienteServicio.obtenerTodosLosClientes(),HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>("No tiene los permisos para solicitar estos datos", HttpStatus.FORBIDDEN);
+    }
    @GetMapping("/api/clientes/actual")
-    public  ClienteDTO obtenerCliente(@RequestParam long id){
-       Cliente cliente = clienteRepositorio.findById(id);
-       return new ClienteDTO(cliente);
+    public  ResponseEntity<Object> obtenerClienteActual(Authentication authentication){
+        Cliente clienteAutenticado = clienteServicio.obtenerClientePorEmail(authentication.getName());
+        return new ResponseEntity<>(new ClienteDTO(clienteAutenticado), HttpStatus.ACCEPTED);
    }
     @PostMapping("/api/clientes")
     public ResponseEntity<Object> registrarCliente(
             @RequestParam String primerNombre,
-            @RequestParam String segundoNombre,
+            @RequestParam(required = false) String segundoNombre,
             @RequestParam String primerApellido,
-            @RequestParam String segundoApellido,
+            @RequestParam(required = false) String segundoApellido,
             @RequestParam String correo,
             @RequestParam String telefono,
             @RequestParam String contraseña) {
 
-        if (clienteRepositorio.findByCorreo(correo) !=  null) {
+        if (clienteServicio.obtenerClientePorEmail(correo) !=  null) {
             return new ResponseEntity<>("El correo electrónico ya está en uso", HttpStatus.FORBIDDEN);
         }
         Cliente nuevoClient = new Cliente(primerNombre, segundoNombre, primerApellido, segundoApellido,correo,telefono,contraseña);
-        clienteRepositorio.save(nuevoClient);
+        clienteServicio.guardarCliente(nuevoClient);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>("Se ha registrado exitosamente",HttpStatus.CREATED);
 
     }
 }
