02eddaac201e74f76c42c48016f37469ac6b966d
Ivan San Miguel <ezequielivan2@gmail.com>
2023-05-30 12:10:00 -0300

    Servicio de cliente y implementacion

---
+++ src/main/java/Macart/Ecommerce/Servicios/ClienteServicio.java
@@ -0,0 +1,17 @@
+package Macart.Ecommerce.Servicios;
+
+import Macart.Ecommerce.DTO.ClienteDTO;
+import Macart.Ecommerce.Modelos.Cliente;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+
+public interface ClienteServicio {
+    List<ClienteDTO> obtenerTodosLosClientes();
+    Cliente obtenerClientePorId(long id);
+    Cliente obtenerClientePorEmail(String email);
+    Cliente obtenerClienteAutenticado(Authentication authentication);
+    boolean isAdmin(Authentication authentication);
+
+    void guardarCliente(Cliente cliente);
+}
