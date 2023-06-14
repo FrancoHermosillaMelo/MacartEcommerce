622448301a8a9950dcc112af9deb1ff1bc009248
Ivan San Miguel <ezequielivan2@gmail.com>
2023-06-14 01:17:33 -0300

    Cree dos propiedades nuevas que son si es unico y la cantidadStockUnico

--- src/main/java/Macart/Ecommerce/Modelos/ProductoTienda.java
+++ src/main/java/Macart/Ecommerce/Modelos/ProductoTienda.java
@@ -24,13 +24,14 @@ public class ProductoTienda {
     private String subCategoria;
     @OneToMany(mappedBy="productoTienda", fetch= FetchType.EAGER)
     private Set<PedidoProducto> pedidoproductos = new HashSet<>();
-
     private boolean activo;
+    private boolean unico;
+    private int cantidadStockUnico;
 
     public ProductoTienda() {
     }
 
-    public ProductoTienda(String nombre, double precio, String descripcion, List<String>imagenesUrl, ProductoTiendaCategoriaGenero categoriaGenero, String subCategoria, boolean activo) {
+    public ProductoTienda(String nombre, double precio, String descripcion, List<String>imagenesUrl, ProductoTiendaCategoriaGenero categoriaGenero, String subCategoria, boolean activo, boolean unico) {
         this.nombre = nombre;
         this.precio = precio;
         this.descripcion = descripcion;
@@ -38,6 +39,20 @@ public class ProductoTienda {
         this.categoriaGenero = categoriaGenero;
         this.subCategoria = subCategoria;
         this.activo = activo;
+        this.unico = unico;
+    }
+
+    /* CONSTRUCTOR PARA ACCESORIOS */
+    public ProductoTienda(String nombre, double precio, String descripcion, List<String> imagenesUrl, ProductoTiendaCategoriaGenero categoriaGenero, String subCategoria, boolean activo, boolean unico, int cantidadStockUnico) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.imagenesUrl = imagenesUrl;
+        this.categoriaGenero = categoriaGenero;
+        this.subCategoria = subCategoria;
+        this.activo = activo;
+        this.unico = unico;
+        this.cantidadStockUnico = cantidadStockUnico;
     }
 
     public ProductoTienda(String nombre) {
@@ -51,88 +66,31 @@ public class ProductoTienda {
     public void agregarTalla(String medida, int cantidad){
         tallas.put(medida, cantidad);
     }
-//    public void actualizarCantidadTalla(String talla, int nuevaCantidad) {
-//        if (tallas.containsKey(talla)) {
-//            tallas.put(talla, nuevaCantidad);
-//        } else {
-//            System.out.println("La talla '" + talla + "' no existe para el producto '" + nombre + "'.");
-//        }
-//    }
 
-    public long getId() {
-        return id;
-    }
+    /* GETTERS */
+    public long getId() {return id;}
+    public String getNombre() {return nombre;}
+    public double getPrecio() {return precio;}
+    public String getDescripcion() {return descripcion;}
+    public Map<String, Integer> getTallas() {return tallas;}
+    public List<String> getImagenesUrl() {return imagenesUrl;}
+    public ProductoTiendaCategoriaGenero getCategoriaGenero() {return categoriaGenero;}
+    public String getSubCategoria() {return subCategoria;}
+    public Set<PedidoProducto> getPedidoproductos() {return pedidoproductos;}
+    public boolean isActivo() {return activo;}
+    public boolean isUnico() {return unico;}
+    public int getCantidadStockUnico() {return cantidadStockUnico;}
 
-    public Map<String, Integer> getTallas() {
-        return tallas;
-    }
-
-    public void setTallas(Map<String, Integer> medidas) {
-        this.tallas = medidas;
-    }
-
-
-    public List<String> getImagenesUrl() {
-        return imagenesUrl;
-    }
-
-    public void setImagenesUrl(List<String> imagenesUrl) {
-        this.imagenesUrl = imagenesUrl;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public ProductoTiendaCategoriaGenero getCategoriaGenero() {
-        return categoriaGenero;
-    }
-
-    public void setCategoriaGenero(ProductoTiendaCategoriaGenero categoriaGenero) {
-        this.categoriaGenero = categoriaGenero;
-    }
-
-    public String getSubCategoria() {
-        return subCategoria;
-    }
-
-    public void setSubCategoria(String subCategoria) {
-        this.subCategoria = subCategoria;
-    }
-
-    public Set<PedidoProducto> getPedidoproductos() {
-        return pedidoproductos;
-    }
-
-    public void setPedidoproductos(Set<PedidoProducto> pedidoproductos) {
-        this.pedidoproductos = pedidoproductos;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
+    /*SETTERS*/
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setPrecio(double precio) {this.precio = precio;}
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+    public void setTallas(Map<String, Integer> tallas) {this.tallas = tallas;}
+    public void setImagenesUrl(List<String> imagenesUrl) {this.imagenesUrl = imagenesUrl;}
+    public void setCategoriaGenero(ProductoTiendaCategoriaGenero categoriaGenero) {this.categoriaGenero = categoriaGenero;}
+    public void setSubCategoria(String subCategoria) {this.subCategoria = subCategoria;}
+    public void setPedidoproductos(Set<PedidoProducto> pedidoproductos) {this.pedidoproductos = pedidoproductos;}
+    public void setActivo(boolean activo) {this.activo = activo;}
+    public void setUnico(boolean unico) {this.unico = unico;}
+    public void setCantidadStockUnico(int cantidadStockUnico) {this.cantidadStockUnico = cantidadStockUnico;}
 }
