14af91d7d4c8305555ad028b233fe1b5d06f3024
Ivan San Miguel <ezequielivan2@gmail.com>
2023-06-14 02:16:35 -0300

    Añadido dos propiedades nuevas unico y cantidadStockUnico

--- src/main/java/Macart/Ecommerce/DTO/ProductoTiendaDTO.java
+++ src/main/java/Macart/Ecommerce/DTO/ProductoTiendaDTO.java
@@ -16,6 +16,8 @@ public class ProductoTiendaDTO {
     private ProductoTiendaCategoriaGenero categoriaGenero;
     private String subCategoria;
     private boolean activo;
+    private boolean unico;
+    private int cantidadStockUnico;
     public ProductoTiendaDTO(ProductoTienda productoTienda) {
         this.id = productoTienda.getId();
         this.nombre = productoTienda.getNombre();
@@ -26,6 +28,8 @@ public class ProductoTiendaDTO {
         this.categoriaGenero = productoTienda.getCategoriaGenero();
         this.subCategoria = productoTienda.getSubCategoria();
         this.activo = productoTienda.isActivo();
+        this.unico = productoTienda.isUnico();
+        this.cantidadStockUnico = productoTienda.getCantidadStockUnico();
     }
 
     public long getId() {
@@ -50,16 +54,15 @@ public class ProductoTiendaDTO {
     public ProductoTiendaCategoriaGenero getCategoriaGenero() {
         return categoriaGenero;
     }
-
     public String getSubCategoria() {
         return subCategoria;
     }
-
     public List<String> getImagenesUrl() {
         return imagenesUrl;
     }
-
     public boolean isActivo() {
         return activo;
     }
+    public boolean isUnico() {return unico;}
+    public int getCantidadStockUnico() {return cantidadStockUnico;}
 }
